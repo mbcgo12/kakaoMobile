7a88ae2eed0655f70542efe659b01d6a0057c197
mbcgo12 <76557695+mbcgo12@users.noreply.github.com>
2021-07-05 15:33:36 +0900

    push1

---
+++ kakao/js/script.js
@@ -0,0 +1,5 @@
+//메뉴
+$('.appBtn').click(function(){
+    $('.bar').toggleClass('addAppbar');
+    $('.mobile_nav').slideToggle(500);
+});
---
+++ kakao/js/swiper.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper('.swiper-container', {
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
